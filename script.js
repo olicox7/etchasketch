3ab15b1bbe25b4e0112402236c1956ff09feef55
Oliver Cox <olicox7@gmail.com>
2020-06-04 21:31:14 +0100

    all modes now fully functioning

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ createGridDivs(16);
 setGridLayout(16);
 
 let gridSquares = document.querySelectorAll(".gridSquare");
-gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseover', addHoverClass)) 
+gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseover', setBlackMode)) 
 
 const resetButton = document.querySelector("#resetButton");
 resetButton.addEventListener("click", resetResize);
@@ -12,7 +12,9 @@ const blackButton = document.querySelector("#blackButton");
 const colorButton = document.querySelector("#colorButton");
 const transparencyButton = document.querySelector("#transparencyButton");
 
+blackButton.addEventListener("click",setBlackMode);
 colorButton.addEventListener("click",setColorMode);
+transparencyButton.addEventListener("click",setTransparentMode);
 
 function createDiv(parentNode){
     newDiv = document.createElement("div");
@@ -41,30 +43,78 @@ function setGridLayout(gridSize){
     width: ${container.clientHeight}px;`);
 }
 
-function addHoverClass(e){
-    this.classList.add("gridSquareHover");
-}
-
 function resetResize(){
     let gridSize = prompt('Enter the number of squares per edge.', '16');
     createGridDivs(gridSize);
     setGridLayout(gridSize);
     gridSquares = document.querySelectorAll(".gridSquare");
-    gridSquares.forEach(gridSquare => gridSquare.classList.remove("gridSquareHover"));
-    gridSquares.forEach(gridSquare => gridSquare.addEventListener('mouseover', addHoverClass));
+    gridSquares.forEach(gridSquare => gridSquare.style["background-color"] = "");
+    if(colorButton.checked) setColorMode()
+    else if (blackButton.checked) setBlackMode()
+    else setTransparentMode()
 }
 
 
 
 function setColorMode(){
     gridSquares = document.querySelectorAll(".gridSquare");
-    gridSquares.forEach(gridSquare => gridSquare.addEventListener("mouseover", randomBackgroundColor))
+    gridSquares.forEach(gridSquare => {
+        if(gridSquare.style["background-color"] != ""){
+            gridSquare.setAttribute("style",`background-color: rgb(${randomInt(255)},${randomInt(255)},${randomInt(255)})`);
+        }
+    });
+    gridSquares.forEach(gridSquare => gridSquare.removeEventListener("mouseover", blackBackgroundColor));
+    gridSquares.forEach(gridSquare => gridSquare.removeEventListener("mouseover", setBlackMode));
+    gridSquares.forEach(gridSquare => gridSquare.addEventListener("mouseover", randomBackgroundColor));
+}
+
+function setBlackMode(){
+    gridSquares = document.querySelectorAll(".gridSquare");
+    gridSquares.forEach(gridSquare => {
+        if(gridSquare.style["background-color"] != ""){
+            gridSquare.setAttribute("style",`background-color: black`);
+        }
+    });
+    gridSquares.forEach(gridSquare => gridSquare.removeEventListener("mouseover", randomBackgroundColor));
+    gridSquares.forEach(gridSquare => gridSquare.removeEventListener('mouseover', setBlackMode));
+    gridSquares.forEach(gridSquare => gridSquare.addEventListener("mouseover", blackBackgroundColor));
 }
 
 function randomInt(maxNumber){
     return Math.floor(Math.random() * (maxNumber + 1));
 }
 
+function blackBackgroundColor(e){
+    this.setAttribute("style","background-color: black")
+}
+
 function randomBackgroundColor(e){
     this.setAttribute("style",`background-color: rgb(${randomInt(255)},${randomInt(255)},${randomInt(255)})`)
+}
+
+function transparentBackgroundColor(e){
+    let transparencyValue = getTransparencyValue(this) || 0;
+    if (transparencyValue == 10) return
+    this.setAttribute("style",`background-color: black; opacity: ${++transparencyValue / 10}`)
+}
+
+function setTransparentMode(){
+    gridSquares = document.querySelectorAll(".gridSquare");
+    gridSquares.forEach(gridSquare => {
+        if(gridSquare.style["background-color"] != ""){
+            gridSquare.setAttribute("style",`background-color: black; opacity: 0.1`);
+        }
+    });
+    gridSquares.forEach(gridSquare => gridSquare.removeEventListener("mouseover", blackBackgroundColor));
+    gridSquares.forEach(gridSquare => gridSquare.removeEventListener("mouseover", randomBackgroundColor));
+    gridSquares.forEach(gridSquare => gridSquare.removeEventListener("mouseover", setBlackMode));
+    gridSquares.forEach(gridSquare => gridSquare.addEventListener("mouseover",transparentBackgroundColor));
+}
+
+function getTransparencyValue(div){
+    let currentValue = div.getAttribute("style","opacity");
+    if (currentValue == "background-color: black; opacity: 1") return 10;
+    if (currentValue == null || Number(currentValue) == NaN) return 0;
+    let newValue = currentValue.slice(-1);
+    return +newValue;
 }
